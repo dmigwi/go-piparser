58fcb6d942cc1f06e3997c722ab53035fdedf601
migwi <migwindungu0@gmail.com>
2019-04-12 02:03:18 +0300

    Update travis

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@ echo -e "GET http://github.com HTTP/1.0\n\n" | nc github.com 80 > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
     echo "System detected internet connectivity"
-    go test -tags fulltest github.com/dmigwi/go-piparser/proposals github.com/dmigwi/go-piparser/proposals/types ./... -v 
+    go test -tags fulltests github.com/dmigwi/go-piparser/proposals github.com/dmigwi/go-piparser/proposals/types ./... -v 
 else
     echo "Ping to github.com failed"
     go test ./... github.com/dmigwi/go-piparser/proposals github.com/dmigwi/go-piparser/proposals/types -v 
